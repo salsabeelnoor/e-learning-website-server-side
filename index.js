dc237fca324e8356cbdd6fe00472bf42dedff434
salsabeelnoor <salsabeelnoor@gmail.com>
2022-10-25 18:42:50 +0600

    category.json done

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
+//process.env.PORT ||
+
+app.get("/", (req, res) => {
+  res.send("E-Learning API running");
+});
+
+app.listen(port, () => {
+  console.log("E- learning website learning on port", port);
+});
