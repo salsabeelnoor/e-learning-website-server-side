68a2ba5d54004446a3e425eb0fc83f7abf5272e4
salsabeelnoor <salsabeelnoor@gmail.com>
2022-10-26 19:08:25 +0600

    api response sent

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 //process.env.PORT ||
+
+app.use(cors());
+const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("E-Learning API running");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course.course_id === id);
+  res.send(selectedCourse);
 });
 
 app.listen(port, () => {
